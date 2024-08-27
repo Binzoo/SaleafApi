7430363d07f5b01d672c07450ba896e231ffbfcc
Binzoo <binamrathapa015@gmail.com>
2024-08-27 02:09:16 +0200

    Add new features or fix bugs

--- obj/Debug/net8.0/SeleafAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SeleafAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleafAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66b5c114ae4dde490898cd563aa16867c840522")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029dd1ca7ec8cd06a947154707cecdfa99a84d0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleafAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleafAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
